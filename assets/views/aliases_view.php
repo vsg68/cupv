7fd40d570e1454622d1f93c367e3b56493066a93
vsg <vsg@gmpro.ru>
2013-06-11 14:57:25 +0400

    вариант с двумя колонками

---
+++ assets/views/aliases_view.php
@@ -0,0 +1,68 @@
+<div class="editmenu">
+	<div id='lb'>ООО "ГАЗМЕТАЛЛПРОЕКТ"</div>
+	<div id='new'></div>
+</div>
+<div class='usrs'>
+	<div id=>
+		<table class='atable some'>
+					<tr><th>alias</th><th colspan=2>mailbox</th></tr>
+
+			   <tr class="key">
+				   <td><?=  'alias_name:' ?></td>
+				   <td>
+					   <input type='hidden' name='alias_st[]' value=''>
+					   <input type='hidden' name='alias_id[]' value=''>
+					   alias_name, alias_name1, alias_name2, alias_name3
+				   </td>
+				   <td><button class='delRow  web'>r</button></td>
+			   </tr>
+
+		</table>
+	</div>
+	<div class='ed'>
+	<table class='some1'>
+					<tr><th>alias</th><th>on/off</th><th>?</th></tr>
+
+			   <tr class="key">
+				   <td><input type='text' name='alias_id[]' value='alias_name1'></td>
+				   <td>
+					   <input type='checkbox' name='alias_st[]' value=''>
+				   </td>
+				   <td><button class='delRow  web'>r</button></td>
+			   </tr>
+			   <tr class="key">
+				   <td><input type='text' name='alias_id[]' value='alias_name2'></td>
+				   <td>
+					   <input type='checkbox' name='alias_st[]' value=''>
+				   </td>
+				   <td><button class='delRow  web'>r</button></td>
+			   </tr>
+			   <tr class="key">
+				   <td><input type='text' name='alias_id[]' value='alias_name3'></td>
+				   <td>
+					   <input type='checkbox' name='alias_st[]' value=''>
+				   </td>
+				   <td><button class='delRow  web'>r</button></td>
+			   </tr>
+			   <tr class="key">
+				   <td><input type='text' name='alias_id[]' value='alias_name'></td>
+				   <td>
+					   <input type='checkbox' name='alias_st[]' value=''>
+				   </td>
+				   <td><button class='delRow  web'>r</button></td>
+			   </tr>
+
+		</table>
+	</div>
+<!--
+	<select id='usrs' class='some' size=32>
+
+			<option value='a' ><div class='key'>mailboxqqqq:</div><div class='value'>aaa</div></option>
+			<option value='a' ><span class='key'>mailboxqqqq:</span><span class='value'>eeeeeeeeeeeee</span></option>
+			<option value='a' ><span class='key'>mailboxqqqq</span><span class='value'>q222</span></option>
+			<option value='a' ><span class='key'>mailbo:</span><span class='value'>1111</span></option>
+
+
+	</select>
+-->
+</div>
--- classes/App/Controller/Aliases.php
+++ classes/App/Controller/Aliases.php
@@ -6,6 +6,25 @@ namespace App\Controller;
 
 class Aliases extends \PHPixie\Controller {
 
+    public function action_index() {
 
+        $view = $this->pixie->view('main');
+		$view->subview = 'aliases_view';
+
+        $view->aliases = $this->pixie->db
+								->query('select')
+								->fields('alias_name','delivery_to')
+								->table('aliases')
+								->execute();
+
+		$view->domains = $this->pixie->db
+								->query('select')
+								->fields('domain_name')
+								->table('domains')
+								->where('delivery_to','virtual')
+								->execute();
+
+        $this->response->body = $view->render();
+    }
 }
 ?>
