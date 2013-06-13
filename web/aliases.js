498dadcfad678d23a6dde1702b3c97fd5288b1bd
vsg <vsg@gmpro.ru>
2013-06-13 16:05:16 +0400

    новая фильтрация через классы и свойство display

--- web/aliases.js
+++ web/aliases.js
@@ -2,28 +2,30 @@ $(function(){
 
 	var tr_aliases = $('tr', '#aliases_box');
 
-	//selected_opts = $(tr_aliases).find('td.key:contains("@t1.gmpro.ru")').parent();
-
 	//Фильтрайия пользователей по домену (alias)
 	$('select','#domains_flt').change(function(){
 
 		filter = $('option:selected', '#domains_flt').text();
 
-		if( filter )
-			selected_opts = $(tr_aliases).find('td.key:contains("@' + filter+ '")').parent();
-		else
-			selected_opts = $(tr_aliases);
+		$('.hidden').removeClass('hidden');
 
-		$('table','#aliases_box').empty().append(selected_opts);
+		if( filter )
+			$('td.key:not(:contains("@' + filter+ '"))', '#aliases_box')
+					.parent()
+					.addClass('hidden');
 
 	});
 
 	$('#fltr').keyup(function(event){
 
-		search_str = $(this).val();
-		selected_opts = $(tr_aliases).find('td.val:contains("' + search_str + '")').parent();
-		$('table','#aliases_box').empty().append(selected_opts);
+		$('.hidden_filter').removeClass('hidden_filter');
 
+		search_str = $(this).val();
+
+		if( search_str )
+			$('td.val:not(:contains("' + search_str + '"))', '#aliases_box')
+			.parent()
+			.addClass('hidden_filter');
 
 	});
 
