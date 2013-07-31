3da876c0bcef361bd571a090ded2622522cf2daa
vsg <vsg@gmpro.ru>
2013-07-31 13:03:11 +0400

    сделан вывод списка разрешенных программ сделана смена картинок на панели программы

--- assets/views/login_view.php
+++ assets/views/login_view.php
@@ -2,8 +2,9 @@
 <div id='logout' title='выход из системы'><strong>&times;</strong></div>
 
 <?php foreach($pages as $page): ?>
-	<div class='theme' title='<?= $page->note ?>' id='<?= strtolower($page->name) ?>'>
-		<p class='name'><a href='<?= $page->link ?>'><?= $page->name ?></a></p>
+	<div class='theme'>
+		<div class='logo' title='<?= $page->note ?>' id='<?= strtolower($page->name) ?>'></div>
+		<div class='name'><a href='<?= $page->link ?>'><?= $page->name ?></a></div>
 	</div>
 <?php endforeach; ?>
 <div class='helper theme'></div>
--- web/login.js
+++ web/login.js
@@ -1,3 +1,8 @@
 $(function(){
 	$('#logout').click(function(){ window.location = '/login/logout';});
+
+	$('.theme').hover(
+					function(){	$('.logo, .name a', this).addClass('hover')},
+					function(){	$('.logo, .name a', this).removeClass('hover')}
+			)
 })
