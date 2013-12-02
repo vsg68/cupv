0021145917987309846cfba0cc881257d7dafd95
Vladimir Gordyunin <vsg@gmpro.ru>
2013-12-02 17:11:56 +0400

    Начали раздел АвторизовПользователи

---
+++ assets/views/auth.php
@@ -0,0 +1,17 @@
+
+		<div id="demo">
+			<table cellpadding="0" cellspacing="0" border="0" class="display" id="tab-aliases">
+				<thead>
+					<tr>
+						<th>login</th>
+						<th>ФИО</th>
+						<th>Пароль</th>
+						<th>Роль</th>
+						<th>Active</th>
+					</tr>
+				</thead>
+			</table>
+		</div>
+
+
+
