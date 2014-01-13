a8d4a433fbe59011e11cc44d7e09ac74ee4dfe09
Vladimir Gordyunin <vsg@gmpro.ru>
2014-01-13 11:24:29 +0400

    Выделение цветом строки где подходит время дедлайна и работает оповещение

--- alarms.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-USER='mailserver'
-PASSWD='boo1aKeisoot'
-BASE='itbase'
-HEADER='\nContent-Type: text/plain; charset=UTF-8'
-
-mysql -N -u $USER -p${PASSWD} -B $BASE < test.sql | awk -F "\t" -v HD="$HEADER" '{system("echo \""$3"\" | mailx -s \""$1 HD"\" "$2)}'
